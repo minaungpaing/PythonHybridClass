7ae22c128230aef2acac14ba76f730e3dc6beb23
DavisDzet <shadowkingmap@gmail.com>
2019-11-03 17:44:43 +0630

    ifstatement

---
+++ ifstatement.py
@@ -0,0 +1,30 @@
+# x = int(input("Please Enter an age :"))
+
+# if x <= 18:
+# 	print("go to School!")
+# elif x > 18 and x <= 50:
+# 	print("go to beach!")
+# elif x > 50 and x <= 75:
+# 	print("go to mornestry!")
+# else:
+# 	print("stay at home!")
+
+# print("I told that \"I don\'t like it!\"")
+# words = ['cat','window', 'defenestrate']
+# for w in words:
+# 	print(w,len(w))
+
+# fruits = ['apple','banana','cucumber','pineapple','orange']
+# print(fruits.index('banana',5))
+
+
+# print(len(fruits))
+
+# for i in range(5):
+# 	print(i)
+
+# a = ['1','2','3','4','5']
+# for i in range(len(a)):
+# 	print(i,a[i])
+# print(sum(range(5)))
+# print(list(range(4)))
