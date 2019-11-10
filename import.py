3e9542601e653f83a966ecf701b79821408b65de
DavisDzet <shadowkingmap@gmail.com>
2019-11-10 17:58:03 +0630

    fibonacci

---
+++ import.py
@@ -0,0 +1,22 @@
+import fib
+
+fib.fib(500)
+
+f = fib.fib
+f(100)
+
+fib.fib(10000)
+
+from fib import fib, fib2
+
+f = fib.fib2(500)
+print(f)
+
+from fib import *
+fib(500)
+
+import fib as fi
+fi.fib(500)
+
+from fib import fib as fibonacci
+fibonacci(500)
