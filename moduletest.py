90b86616f30d0c0d491d60d87c2416ea48e4d24d
DavisDzet <shadowkingmap@gmail.com>
2019-12-15 17:33:40 +0630

    module

---
+++ moduletest.py
@@ -0,0 +1,15 @@
+import mymodule
+
+x = mymodule.student["code"]
+a = mymodule.student["name"]
+y = mymodule.student1["mother"]
+b = mymodule.student1["father"]
+z = mymodule.student2["age"]
+c = mymodule.student2["class"]
+
+print(a)
+print(x)
+print(y)
+print(b)
+print(z)
+print(c)
