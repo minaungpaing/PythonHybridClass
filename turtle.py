a52c322fa8bdcea6eb5ae82f8ede1edafa5a7e19
DavisDzet <shadowkingmap@gmail.com>
2020-01-19 17:36:44 +0630

    'Input/Output'

---
+++ turtle.py
@@ -0,0 +1,21 @@
+# from turtle import *
+
+# color('red','yellow')
+# begin_fill()
+# while True:
+# 	forward(200)
+# 	left(170)
+# 	if abs(pos()) < 1:
+# 		break
+# end_fill()
+# done()
+
+import turtle
+
+triple = turtle.turtle()
+
+for i in range(20):
+	triple.forward(i * 10 )
+	triple.right(144)
+
+turtle.done()
